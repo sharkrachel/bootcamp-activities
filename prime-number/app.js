41d9fe44d7df07d152733a82fdb1cafa79920716
Rachel Sipes <rasipes@gmail.com>
2020-01-04 14:49:39 -0500

    added prime number js

---
+++ prime-number/app.js
@@ -0,0 +1,12 @@
+function isPrimeNumber(n) {
+    for (var i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+
+    }
+    return n > 1;
+}
+
+
+console.log(isPrimeNumber(73));
