cabc5181bfb3e2940636e8684cbca5495fb5567a
Rachel Sipes <rasipes@gmail.com>
2020-02-06 20:47:16 -0500

    added instructions

--- node-activity/nodeapp.js
+++ node-activity/nodeapp.js
@@ -1,3 +1,8 @@
+// Instructions:
+// Starting from a blank JavaScript file, create a Node application that takes in two command line arguments and checks if they are equal. If the two numbers are equal then output: "true". Otherwise output: "false".
+// Bonus: Check if the two numbers are both multiples of 7. Again output "true" if they are and output "false" if they are not.
+// Hint: console.log(process.argv) as a starting point if you are completely lost.
+
 console.log(process.argv);
 
 var input1 = process.argv[2]
